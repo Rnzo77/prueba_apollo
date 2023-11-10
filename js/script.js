0a9608c0fe4a5c6e4f9c48c3af99cb9b4ff1e23f
Renzo Loli <renzo.loli.7@gmail.com>
2023-11-09 20:23:22 -0500

    Agregados la pagina de proyectos, y otros detalles

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//sidenav
+
+function openNav() {
+    document.getElementById("mySidenav").style.cssText = "width: 33.333%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.cssText = "width: 0";
+  }
