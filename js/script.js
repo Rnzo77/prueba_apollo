5720e185fe529d66d4f965a808aa5f30fb62f6f3
Renzo Loli <renzo.loli.7@gmail.com>
2023-11-10 23:10:02 -0500

    responsive en la pagina de proyectos, ya solo queda terminar las paginas que faltan

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.cssText = "width: 33.333%";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("mySidenav").style.cssText = "width: 0";
-  }
+}
+
+function openNavPortrait() {
+    document.getElementById("mySidenav").style.cssText = "height: 100%";
+}
+
+function closeNavPortrait() {
+    document.getElementById("mySidenav").style.cssText = "height: 0";
+}
